00d87cdd73eeb31cb46e9cebb8c6644227a72c1f
wlx-macbook <fifa0329@gmail.com>
2015-01-10 14:14:05 +0800

    try to commit using -a

--- SameTree.java
+++ SameTree.java
@@ -7,14 +7,61 @@
  */
 public class SameTree {
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if (p == null && q == null)
-			return true;
-		if ((p == null && q != null) || (p != null && q == null))
-			return false;
-		if (p.val != q.val)
-			return false;
-		else {
-			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-		}
-	}
+        if (p == null && q == null) {
+            return true;
+        } else if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        else  {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        if (p == null && q == null)
+            return true;
+        if ((p == null && q != null) || (p != null && q == null))
+            return false;
+        if (p.val != q.val)
+            return false;
+        else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        */
