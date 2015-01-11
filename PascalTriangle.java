4014aefaefe64a0bcc1c920d82b8cb699aae235d
wlx-macbook <fifa0329@gmail.com>
2015-01-11 21:02:52 +0800

    PascalTriangle1

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,5 +1,5 @@
 
-
+import java.util.List;
 import java.util.ArrayList;
 
 /**
@@ -17,7 +17,71 @@ import java.util.ArrayList;
  */
 
 public class PascalTriangle {
-	public ArrayList<ArrayList<Integer>> pascalTriangle(int numRows) {
+	public List<List<Integer>> pascalTriangle(int numRows) {
+		//的确和我想的不一样
+		
+		
+		//我的想法是1的时候返回1
+		//2的时候返回1 11
+		//然后我知道规律 i 的上面的 i+ i-1
+		
+		
+		
+		//答案思路不一样
+		//仔细看了一下 思路是一样的 只不过他是先用1全部填满所有值
+		
+		
+		//然后再利用刚刚的规律去填值
+		//不得不说 要比我的方法简练 通过for 来代替我的 1 2
+		
+		
+		
+		
+		
+		//这一题我没有选择自己做
+		//因为思路比较清晰
+		//但是复制答案的时候出现了编译上面的错误，不能转换为List
+
+		//我将答案改成了以下，就可以过了
+
+		List<List<Integer>> pt = new ArrayList<List<Integer>>();
+		int k = 1;
+		for (int i = 0; i < numRows; i++) {
+			List<Integer> r = new ArrayList<Integer>();
+			for (int j = 0; j < k; j++) {
+				r.add(1);
+			}
+			k++;
+			pt.add(r);
+		}
+		for (int f = 2; f < pt.size(); f++) {
+			List<Integer> prev = pt.get(f - 1);
+			List<Integer> current = pt.get(f);
+			for (int g = 1; g < current.size() - 1; g++) {
+				current.set(g, prev.get(g - 1) + prev.get(g));
+			}
+		}
+		return pt;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		
+		
 		ArrayList<ArrayList<Integer>> pt = new ArrayList<ArrayList<Integer>>();
 		int k = 1;
 		for (int i = 0; i < numRows; i++) {
@@ -36,5 +100,7 @@ public class PascalTriangle {
 			}
 		}
 		return pt;
+		*/
+		
 	}
 }
