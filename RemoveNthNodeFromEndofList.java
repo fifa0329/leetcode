1fb956a71a65b887ca144720494d706445582651
wlx-macbook <fifa0329@gmail.com>
2015-01-14 23:15:49 +0800

    好不容易过了这个remove nth reverse

--- RemoveNthNodeFromEndofList.java
+++ RemoveNthNodeFromEndofList.java
@@ -37,7 +37,8 @@ public class RemoveNthNodeFromEndofList {
         
         
         //head应该指向1
-        
+        //参考答案
+//        
 //        ListNode root = new ListNode(0);
 //        ListNode fast = root;
 //        ListNode slow = root;
@@ -62,10 +63,12 @@ public class RemoveNthNodeFromEndofList {
         
         //此题说明了：如何单方向取反向nth
 
-       
 
-        int i=1;
+
         
+        //我的方法，纯数学的想法来做，出现了很多的例外case，不得不佩服测试
+        
+        int i=1;
         ListNode pointer=head;
         while (pointer.next!=null)
         {
@@ -73,21 +76,59 @@ public class RemoveNthNodeFromEndofList {
             pointer=pointer.next;
         }
 
-        
+        //特殊情况
+        // 如果长度为1，输入合理的话必然也为1，这里思考的实际上不够严密，是在赌博参数正确
+        if (i==1)
+        {
+            return null;
+        }
+
+        //特殊情况
+        //如果取长度的n
+        //会导致原本的丢失
+        if(n==i)
+        {
+            return head.next;
+
+        }
+
+
         ListNode toDelete=head;
-        for (int j=0;j<i-n;j++)
+        for (int j=0;j<i-n-1;j++)
         {
             toDelete=toDelete.next;
         }
         //做了i-n-1次移位，到达了n+1的位置上
-        
-        
+
+        //特殊情况
+        //有可能不存在一个很好的toDelete.next=toDelete.next.next;结构
+        if(toDelete.next==null)
+        {
+            return head;
+        }
+
+
         toDelete.next=toDelete.next.next;
-        
+
 
         return head;
 
+    }
 
+//
+//        Submission Result: Runtime ErrorMore Details
+//
+//        Runtime Error Message:	Line 32: java.lang.NullPointerException
+//        Last executed input:	{1}, 1
+//          这个没有过
+        
+        
+        //连续改了好几次 都不对 
+        //陷入了细节之中，肯定过不了了
+        
+        //为什么我用数学的方法会错那？？？
+        //1，我没有考虑到null
+        //2，（1，2）2
 
 
 
