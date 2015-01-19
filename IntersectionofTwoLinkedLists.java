e1f59a4d7cd5097a1d16b1abc2316e45a6fa597e
wlx-macbook <fifa0329@gmail.com>
2015-01-20 00:02:46 +0800

    add

--- IntersectionofTwoLinkedLists.java
+++ IntersectionofTwoLinkedLists.java
@@ -21,6 +21,9 @@
  You may assume there are no cycles anywhere in the entire linked structure.
  Your code should preferably run in O(n) time and use only O(1) memory.* *
  */
+
+import java.util.HashSet;
+
 public class IntersectionofTwoLinkedLists {
 
     /**
