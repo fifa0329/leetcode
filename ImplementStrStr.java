3ef521381d87e70f1b46a7927f70106c8db0eb19
wlx-macbook <fifa0329@gmail.com>
2015-01-22 21:56:44 +0800

    implement strstr

--- ImplementStrStr.java
+++ ImplementStrStr.java
@@ -1,33 +1,93 @@
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 /**
- * Implement strStr().
- * <p/>
- * Returns a pointer to the first occurrence of needle in haystack, or null if
- * needle is not part of haystack.
+ Implement strStr().
+
+ Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ Update (2014-11-02):
+ The signature of the function had been updated to return the index instead of the pointer. If you still see your function signature returns a char * or String, please click the reload button  to reset your code definition.
  */
 
 public class ImplementStrStr {
-    public String strStr(String haystack, String needle) {
-        int i = 0;
-        int len1 = haystack.length();
-        int len2 = needle.length();
-        if (len1 == 0 && len2 == 0)
-            return "";
-        for (; i < len1; i++) {
-            int a = i;
-            int b = 0;
-            boolean match = true;
-            while (b < len2) {
-                if (a >= len1 || haystack.charAt(a) != needle.charAt(b)) {
-                    match = false;
+
+
+    public int strStr(String haystack, String needle) {
+
+        
+        //下面是我自己写的答案
+
+        int pointer=0;
+
+        char[] haystackchars=haystack.toCharArray();
+        char[] needlechars=needle.toCharArray();
+        
+        
+        while (pointer<=haystackchars.length-needlechars.length)
+        {
+            int i=0;
+            for (i = 0; i < needlechars.length; i++)
+            {
+                if (haystackchars[pointer+i]!=needlechars[i])
+                {
+                    pointer++;
                     break;
-                } else {
-                    a++;
-                    b++;
                 }
             }
-            if (match)
-                return haystack.substring(i);
+            if (i==needlechars.length)
+            {
+                return pointer;
+            }
         }
-        return null;
+        
+
+        return -1;
+        
+        //同时我解锁了
+        //O(nm) runtime, O(1) space – Brute force:
+
+//        You could demonstrate to your interviewer that this problem can be solved using known efficient algorithms such as Rabin-Karp algorithm, KMP algorithm, and the Boyer- Moore algorithm. Since these algorithms are usually studied in an advanced algorithms class, it is sufficient to solve it using the most direct method in an interview – The brute force method.
+//
+//        The brute force method is straightforward to implement. We scan the needle with the haystack from its first position and start matching all subsequent letters one by one. If one of the letters does not match, we start over again with the next position in the haystack.
+//
+//        The key is to implement the solution cleanly without dealing with each edge case separately.
+//
+//                Cancel rating12345
+//        Average Rating: 3.7 (113 votes)
+//
+//        Is this solution helpful? Read our book to learn more.
+
+
+
+
+
+
+
+        
+
+
+//        没怎么看标准答案是怎么写的
+//        int i = 0;
+//        int len1 = haystack.length();
+//        int len2 = needle.length();
+//        if (len1 == 0 && len2 == 0)
+//            return "";
+//        for (; i < len1; i++) {
+//            int a = i;
+//            int b = 0;
+//            boolean match = true;
+//            while (b < len2) {
+//                if (a >= len1 || haystack.charAt(a) != needle.charAt(b)) {
+//                    match = false;
+//                    break;
+//                } else {
+//                    a++;
+//                    b++;
+//                }
+//            }
+//            if (match)
+//                return haystack.substring(i);
+//        }
+//        return null;
     }
 }
---
+++ ValidSudoku.java
@@ -0,0 +1,31 @@
+import java.util.Hashtable;
+
+/**
+ * Created by wlx on 1/22/15.
+ * 
+ * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+ The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+
+ A partially filled sudoku which is valid.
+
+ Note:
+ A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+
+ Show Tags
+ Have you met this question in a real interview? Yes  No
+ Discuss
+
+ * * 
+ */
+public class ValidSudoku
+{
+
+
+    public boolean isValidSudoku(char[][] board) {
+
+
+        Hashtable<Integer,Integer>
+    }
+}
