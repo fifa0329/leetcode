e01bc7195e9d8fd39a86a4bacc9aaf1c05c6d28f
lingxiang.wang <lingxiang.wang@autonavi.com>
2015-04-08 11:09:18 +0800

    LengthofLastWord.java

--- LengthofLastWord.java
+++ LengthofLastWord.java
@@ -11,7 +11,34 @@
  */
 
 public class LengthofLastWord {
+
     public int lengthOfLastWord(String s) {
+        //第一次尝试
+//        if (s.isEmpty()||s==null)
+//            return 0;
+//
+//        String news=s+" ";
+//        String[] every=news.split(" ");
+//        String result=every[every.length-1];
+//        return result.length()-1;
+//
+//        Submission Result: Runtime ErrorMore Details
+//
+//        Runtime Error Message:	Line 8: java.lang.ArrayIndexOutOfBoundsException: -1
+//        Last executed input:	" "
+//        确实根本不回想到这种只有“ ”的情况
+//        特殊情况有很多
+//        （1）        “ ”
+//        （2）hello world“   ”
+//        （3）hello     world
+
+
+//        我发现我的思路一般只能想到最完美的情况
+//        看答案
+
+//        它的思路就是找到最后一个单词头的索引，以及尾的索引
+
+
         int i = s.length() - 1;
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
